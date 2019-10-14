274bcd1f326ad0a5202ef253e43bfb16af7d3694
titanium901 <nemes901@hotmail.com>
2019-10-14 14:55:14 +0300

    delete mainStoryboard

--- SnapKIt/AppDelegate.swift
+++ SnapKIt/AppDelegate.swift
@@ -10,11 +10,18 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        let vc = ViewController()
+        let navController = UINavigationController(rootViewController: vc)
+        window?.rootViewController = navController
+        
         return true
     }
 
